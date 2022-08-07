f06dd9b5c4d26e62cfefb41d2a50708c0890b822
bcollazo <bcollazo@emeal.nttdata.com>
2022-08-07 13:54:47 -0500

    mensaje de equipo

---
+++ OrdenBurbuja/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.SimpleTimeZone;
+
+public class Main {
+    public static void main(String [] args){
+        int num[] = {10,-2,8,30,1,30};
+        int temp ;
+        for (int j=0; j < num.length-1;j++){
+            for (int i=0; i < num.length-1;i++) {
+                //System.out.println(i);
+                //System.out.println("el valor actual es: "+num[i]+" y el valor Siguiente es: "+num[i+1]);
+                if (num[i] > num[i + 1]) {
+                    temp = num[i];
+                    num[i] = num[i + 1];
+                    num[i + 1] = temp;
+                }
+            }
+        }
+        System.out.println(num[0]+" "+ num[1] +" "+num[2]+" "+num[3]+" "+num[4]+" "+num[5]);
+    }
+}
